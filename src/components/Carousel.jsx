ae0f47279a7b4705eb1bd202d6ea02434e05cbc5
Mohana Vamsi <mohanavamsi14@gmail.com>
2024-02-05 09:38:52 +0530

    Add files via upload

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -14,15 +14,18 @@ function Carousel() {
             <h1 className="title">{image[curr].title}</h1>
             <h2 className="des">{image[curr].subtitle}</h2>
             <div className="bu">
+            <div className="no">
             <ArrowBackIosIcon onClick={()=>{
                 let check = curr == 0 ? 2 : curr-1;
                 setCurr(check)
-            }} className="no"/>
-
+            }} className="n"/>
+            </div>
+            <div className="no">
             <ArrowForwardIosIcon onClick={()=>{
                 let check=curr == 2 ? 0 : curr+1;
                   setCurr(check);
-            }} className="no"/>
+            }} className="n"/>
+            </div>
              </div>
         </>
     )
